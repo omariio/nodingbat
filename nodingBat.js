6d612b64c71f3af4f0f8cb55b49cab243a3719d9
mfukano <fukano.mat@gmail.com>
2015-03-14 22:01:41 -0700

    Beginning interpretergit add .git add .! yayayaya

--- nodingBat.js
+++ nodingBat.js
@@ -5,24 +5,71 @@ if (Meteor.isClient) {
 var myEditor;
 
 Template.hello.helpers({
+  "editorOptions": function() {
+        return {
+            lineNumbers: true,
+            mode: "javascript"
+        }
+    },
 
-    renderEditor: function () {
-    return function(editor) {
-      myEditor = editor;
+    "editorCode": function() {
+        return "Code to show in editor";
     }
-  }
 
 });
 
 
-  Template.hello.events({
-    "some event": function(e, t) {
-        var code = t.find("jcode").value;
-        alert(code);
-    }
-});
+Template.hello.rendered = function () {
+  Session.set("varName", "diff21 = function(n) {\n" +
+      "\tif (n <= 21)\n" +
+      "\t\treturn 21 - n;\n" + 
+      "\telse\n" + 
+      "\t\treturn (n - 21) * 2;\n" + 
+      "} \n\n");
+      // "console.log(diff21(19));");
 }
 
+
+Template.hello.events({
+  "getEditorText": function() {
+      return Session.get("varName"); // "varName" is variable name you provided to reactiveVar 
+  },
+
+  'click button': function(){
+      var obj = eval("(" + Session.get("varName") + ")");
+      for(i = 0; i < 5; i++){
+        if(obj(i) == diff21(i))
+          console.log("great success! (obj: ", obj(i), "==  diff21: ", diff21(i),")");
+        else 
+          console.log("nice try");
+      }
+  }
+
+      // console.log(obj);
+      // console.log(v);
+      //   if (obj != (21 - v))
+      //   console.log("wrong");
+      // else console.log("hooray");
+
+
+
+  // "some event": function(e, t) {
+  //     console.log(e);
+  //     console.log(t);
+  //     var code = t.find("#some-id").value;
+  //     alert(code);
+  //   }
+
+});
+
+}
+
+var diff21 = function(n) {
+  if (n <= 21)
+    return 21 - n;
+  else
+    return (n - 21) * 2;
+} 
 
 
 if (Meteor.isServer) {
