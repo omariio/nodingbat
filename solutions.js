a4498ea71122e6769296b4e470f8c2f52dcd8414
endeav0ur <tylsmith@gmail.com>
2015-04-21 18:17:07 -0700

    added soln for front3

--- solutions.js
+++ solutions.js
@@ -366,6 +366,25 @@ var notString = function(str){
 	}
 	return "not " + str;
 }
-console.log(notString("candy"));
-console.log(notString("x"));
-console.log(notString("not bad"));
+// console.log(notString("candy"));
+// console.log(notString("x"));
+// console.log(notString("not bad"));
+
+// Java > Warmup-1 > front3
+// Given a string, we'll say that the front is the first 3 chars of the string. 
+// If the string length is less than 3, the front is whatever is there. 
+// Return a new string which is 3 copies of the front. 
+
+var front3 = function(str){
+	front = "";
+	if (str.length >= 3){
+		front = str.substring(0,3);
+	}
+	else {
+		front = str;
+	}
+	return front + front + front;
+}
+console.log(front3("Java"));
+console.log(front3("Chocolate"));
+console.log(front3("abc"));
