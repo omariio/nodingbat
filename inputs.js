9480e98bd1fd2f3b544b9e2f3651e5d4720a36ea
Tyler Smith <tylsmith@gmail.com>
2015-04-22 20:09:06 -0700

    inputs.js

---
+++ inputs.js
@@ -0,0 +1,12 @@
+data = {
+  "sleepIn":{
+    "inputs":[
+      "(true, true)", 
+      "(true, false)", 
+      "(false, true)",
+      "(false, false)"
+    ],
+    "setup":"var sleepIn = function(weekday, vacation){ }",
+    "divineFunction":"var divine = function(weekday, vacation){ return !weekday || vacation; }"
+  }
+}
