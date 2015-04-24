e3b8eee5f9acf5a1ccb9fca8b3daa23de46e852e
michaelbetts <mwbetts@ucsc.edu>
2015-04-24 04:53:18 -0700

    made it super sexy and more of a single page app

--- inputs.js
+++ inputs.js
@@ -6,7 +6,7 @@ data = {
       "(false, true)",
       "(false, false)"
     ],
-    "setup":"var sleepIn = function(weekday, vacation){ }",
+    "setup":"/*The parameter weekday is True if it is a weekday, and the parameter vacation*/ \n var sleepIn = function(weekday, vacation){ }",
     "divineFunction":"var divine = function(weekday, vacation){ return !weekday || vacation; }"
   }
 }
