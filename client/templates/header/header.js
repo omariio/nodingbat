a9814f164f09839feb996b9557d2aae828ce2776
cmehrabian <cameron.mehrabian@gmail.com>
2015-04-29 14:32:09 -0700

    headerUpdate

---
+++ client/templates/header/header.js
@@ -0,0 +1,8 @@
+Template.header.rendered=function(){
+  if(!Meteor.user()){
+    var a = $('.dropdown-toggle').html(" ");
+    a.append("<span class='glyphicon glyphicon-user'></span>")
+  }else{
+    $('#login-buttons-logout').before('<a href="/account" class="account-link button">My Account</a>');
+  }
+};
