9318c07b81d8c0411442187afe3a918b74931472
michaelbetts <mwbetts@ucsc.edu>
2015-04-29 21:08:56 -0700

    updated database up to missingChar

--- server/server.js
+++ server/server.js
@@ -30,16 +30,14 @@
                 ],
             "parameters": "sleepIn(weekday, vacation) ", 
             "setup":"/* The parameter weekday is True if it is a weekday, and the parameter */\n/* vacation is True if we are on vacation. We sleep in if it is not a weekday */\n/* or we're on vacation. Return True if we sleep in. */\n\nvar sleepIn = function(weekday, vacation){ \n\n}"
-            // "divineFunction":"var divine = function(weekday, vacation){ return !weekday || vacation; }"
       });
-    
         Posts.insert({
             "section":"warm-up1",          
           	"name":"diff21",
               "inputs":[
                 "(19)", 
                 "(10)", 
-                "(21)",
+                "(21)"
                 ],
             "parameters": "diff21(n) ", 
           "setup":"/* Given an int n, return the absolute difference between n and 21, */\n/* except return double the absolute difference if n is over 21. */\nvar diff21 = function(n){ \n\n}"
@@ -50,23 +48,22 @@
               "inputs":[
                 "(93)", 
                 "(90)", 
-                "(89)",
+                "(89)"
                 ],
-                "parameters": "nearHundred(n)- ", 
-          "setup":"/* Given an int n, return True if it is within 10 of 100 or 200. */\n/* Note: abs(num) computes the absolute value of a number. */\nnearhundred = function(n){ \n\n}",
-          "divineFunction":"var nearHundred = function(n) {if (Math.abs(100) - n <= 10 || Math.abs(200) - n <= 10){return true;}else{return false;}}"
+            "parameters": "nearHundred(n) ",
+          "setup":"/* Given an int n, return True if it is within 10 of 100 or 200. */\n/* Note: abs(num) computes the absolute value of a number. */\nnearhundred = function(n){ \n\n}"
         });
+
         Posts.insert({
             "section":"warm-up1",          
             "name":"missingChar",
               "inputs":[
-                "(true, true)", 
-                "(true, false)", 
-                "(false, true)",
-                "(false, false)"
+                "('kitten', 1)", 
+                "('kitten', 0)",
+                "('kitten', 4)"
                 ],
-          "setup":"/*Given an int n, return the absolute difference between n and 21,\nexcept return double the absolute difference if n is over 21.*/\nvar diff21=function(n){ }",
-          "divineFunction":"var diff21 = function(n){ if (n <= 21){ return 21 - n;} else {return (n - 21) * 2;}}"
+                "parameters": "missingChar(str, n) ",
+          "setup":"/* Given a non-empty string and an int n, return a new string */\n/* where the char at index n has been removed. The value of n */\n/* will be a valid index of a char in the original string */\n/* (i.e. n will be in the range 0..str.length()-1 inclusive). */\nmissingChar = function(str, n){ \n\n}"
         });
         Posts.insert({
             "section":"warm-up1",          
@@ -77,8 +74,8 @@
                 "(false, true)",
                 "(false, false)"
                 ],
+                "parameters": "backAround(str, n) ",
           "setup":"/*Given an int n, return the absolute difference between n and 21,\nexcept return double the absolute difference if n is over 21.*/\nvar diff21=function(n){ }",
-          "divineFunction":"var diff21 = function(n){ if (n <= 21){ return 21 - n;} else {return (n - 21) * 2;}}"
         });
         Posts.insert({
             "section":"warm-up1",          
