65f0a0b93c1da3594e98d70ab663fd6d6ae2f003
cmehrabian <cameron.mehrabian@gmail.com>
2015-05-03 18:00:53 -0700

    success/failure session variables

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@
 
         var messageObject = {
           body:body,
-          modalID:ceID,   
+          modalID:ceID,
           user:Meteor.user(),
           timestamp:(new Date()).getTime()
         }
@@ -23,31 +23,31 @@
             "section":"warm-up1",
           	"name":"sleepIn",
               "inputs":[
-                "(true, true)", 
-                "(true, false)", 
+                "(true, true)",
+                "(true, false)",
                 "(false, true)",
                 "(false, false)"
                 ],
-            "parameters": "sleepIn(weekday, vacation) ", 
+            "parameters": "sleepIn(weekday, vacation) ",
             "setup":"/* The parameter weekday is True if it is a weekday, and the parameter */\n/* vacation is True if we are on vacation. We sleep in if it is not a weekday */\n/* or we're on vacation. Return True if we sleep in. */\n\nvar sleepIn = function(weekday, vacation){ \n\n}"
       });
         Posts.insert({
-            "section":"warm-up1",          
+            "section":"warm-up1",
           	"name":"diff21",
               "inputs":[
-                "(19)", 
-                "(10)", 
+                "(19)",
+                "(10)",
                 "(21)"
                 ],
-            "parameters": "diff21(n) ", 
+            "parameters": "diff21(n) ",
           "setup":"/* Given an int n, return the absolute difference between n and 21, */\n/* except return double the absolute difference if n is over 21. */\nvar diff21 = function(n){ \n\n}"
         });
         Posts.insert({
-            "section":"warm-up1",          
+            "section":"warm-up1",
             "name":"nearHundred",
               "inputs":[
-                "(93)", 
-                "(90)", 
+                "(93)",
+                "(90)",
                 "(89)"
                 ],
             "parameters": "nearHundred(n) ",
@@ -55,10 +55,10 @@
         });
 
         Posts.insert({
-            "section":"warm-up1",          
+            "section":"warm-up1",
             "name":"missingChar",
               "inputs":[
-                "('kitten', 1)", 
+                "('kitten', 1)",
                 "('kitten', 0)",
                 "('kitten', 4)"
                 ],
@@ -66,11 +66,11 @@
           "setup":"/* Given a non-empty string and an int n, return a new string */\n/* where the char at index n has been removed. The value of n */\n/* will be a valid index of a char in the original string */\n/* (i.e. n will be in the range 0..str.length()-1 inclusive). */\nmissingChar = function(str, n){ \n\n}"
         });
         Posts.insert({
-            "section":"warm-up1",          
+            "section":"warm-up1",
             "name":"backAround",
               "inputs":[
-                "(true, true)", 
-                "(true, false)", 
+                "(true, true)",
+                "(true, false)",
                 "(false, true)",
                 "(false, false)"
                 ],
@@ -78,11 +78,11 @@
           "setup":"/*Given an int n, return the absolute difference between n and 21,\nexcept return double the absolute difference if n is over 21.*/\nvar diff21=function(n){ }",
         });
         Posts.insert({
-            "section":"warm-up1",          
+            "section":"warm-up1",
             "name":"startHi",
               "inputs":[
-                "(true, true)", 
-                "(true, false)", 
+                "(true, true)",
+                "(true, false)",
                 "(false, true)",
                 "(false, false)"
                 ],
@@ -90,11 +90,11 @@
           "divineFunction":"var diff21 = function(n){ if (n <= 21){ return 21 - n;} else {return (n - 21) * 2;}}"
         });
         Posts.insert({
-            "section":"warm-up1",          
+            "section":"warm-up1",
             "name":"hasTeen",
               "inputs":[
-                "(true, true)", 
-                "(true, false)", 
+                "(true, true)",
+                "(true, false)",
                 "(false, true)",
                 "(false, false)"
                 ],
@@ -102,11 +102,11 @@
           "divineFunction":"var diff21 = function(n){ if (n <= 21){ return 21 - n;} else {return (n - 21) * 2;}}"
         });
         Posts.insert({
-            "section":"warm-up1",          
+            "section":"warm-up1",
             "name":"startHi",
               "inputs":[
-                "(true, true)", 
-                "(true, false)", 
+                "(true, true)",
+                "(true, false)",
                 "(false, true)",
                 "(false, false)"
                 ],
@@ -114,11 +114,11 @@
           "divineFunction":"var diff21 = function(n){ if (n <= 21){ return 21 - n;} else {return (n - 21) * 2;}}"
         });
         Posts.insert({
-            "section":"warm-up1",          
+            "section":"warm-up1",
             "name":"mixStart",
               "inputs":[
-                "(true, true)", 
-                "(true, false)", 
+                "(true, true)",
+                "(true, false)",
                 "(false, true)",
                 "(false, false)"
                 ],
@@ -126,11 +126,11 @@
           "divineFunction":"var diff21 = function(n){ if (n <= 21){ return 21 - n;} else {return (n - 21) * 2;}}"
         });
         Posts.insert({
-            "section":"warm-up1",          
+            "section":"warm-up1",
             "name":"close10",
               "inputs":[
-                "(true, true)", 
-                "(true, false)", 
+                "(true, true)",
+                "(true, false)",
                 "(false, true)",
                 "(false, false)"
                 ],
@@ -138,11 +138,11 @@
           "divineFunction":"var diff21 = function(n){ if (n <= 21){ return 21 - n;} else {return (n - 21) * 2;}}"
         });
         Posts.insert({
-            "section":"warm-up1",          
+            "section":"warm-up1",
             "name":"stringE",
               "inputs":[
-                "(true, true)", 
-                "(true, false)", 
+                "(true, true)",
+                "(true, false)",
                 "(false, true)",
                 "(false, false)"
                 ],
@@ -150,11 +150,11 @@
           "divineFunction":"var diff21 = function(n){ if (n <= 21){ return 21 - n;} else {return (n - 21) * 2;}}"
         });
         Posts.insert({
-            "section":"warm-up1",          
+            "section":"warm-up1",
             "name":"everyNth",
               "inputs":[
-                "(true, true)", 
-                "(true, false)", 
+                "(true, true)",
+                "(true, false)",
                 "(false, true)",
                 "(false, false)"
                 ],
@@ -162,11 +162,11 @@
           "divineFunction":"var diff21 = function(n){ if (n <= 21){ return 21 - n;} else {return (n - 21) * 2;}}"
         });
         Posts.insert({
-            "section":"warm-up1",          
+            "section":"warm-up1",
             "name":"monkeyTrouble",
               "inputs":[
-                "(true, true)", 
-                "(true, false)", 
+                "(true, true)",
+                "(true, false)",
                 "(false, true)",
                 "(false, false)"
                 ],
