93aec079f08991e567949ab69cce0af9319d167a
michaelflops <mwbetts@ucsc.edu>
2015-11-24 00:45:19 -0800

    account buttons added

--- client/templates/header/header.js
+++ client/templates/header/header.js
@@ -1,14 +1,20 @@
 Template.header.helpers({
   username: function(){
   	return Meteor.users.findOne().username;
+  },
+  section: function(){
+  	return Meteor.exercises.find({section: section});
   }
 });
 
 Template.header.rendered=function(){
+  $(".button-collapse").sideNav();
+  $('.modal-trigger').leanModal();
+  $('#aside').pushpin({ top:110, bottom:500 });
   if(!Meteor.user()){
     var a = $('.dropdown-toggle').html(" ");
     a.append("<span class='glyphicon glyphicon-user'></span>")
   }else{
-    $('#login-buttons-logout').before('<a href="/account" class="account-link button">My Account</a>');
+    $('#login-buttons-logout').before('<a href="/profile" class="account-link button">My Account</a>');
   }
 };
