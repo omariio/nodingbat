af16f7752870512449c40f7f121bfbb9cfb1a6d1
Asha <akarim@ucsc.edu>
2015-12-22 22:31:08 -0800

    fixed stringE issue

--- solutions.js
+++ solutions.js
@@ -126,7 +126,7 @@ solutions.close10 = function(a, b){
 
 // Java > Warm-up1 > stringE
 // Return true if the given string contains between 1 and 3 'e' chars.
-solutions.tringE = function(str){
+solutions.stringE = function(str){
 	count = 0;
 	for(var i=0; i<str.length; i++){
 		if(str.charAt(i) == "e"){
